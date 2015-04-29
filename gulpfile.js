746a0864def59fe01f318b359d7b4592dfadf8dd
eldad fux <eldad.fux@gmail.com>
2015-04-29 22:20:39 +0300

    Added 4 tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,35 @@
 // Include gulp
-var gulp    = require('gulp');
+var gulp    = require('gulp'),
 
-// Include plugins
-var uglify  = require('gulp-uglify');
-var concat  = require('gulp-concat');
-var rename  = require('gulp-rename');
+    // Plugins
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat'),
+    rename  = require('gulp-rename'),
+    watch   = require('gulp-watch'),
 
-// Config
-var mainFile = './litespeed.js';
+    // Config
+    config  = {
+        mainFile: './litespeed.js'
+    }
+;
 
 gulp.task('concat', function() {
     return gulp.src('src/**/*.js')
-        .pipe(concat(mainFile))
+        .pipe(concat(config.mainFile))
         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('uglify', function() {
-    return gulp.src(mainFile)
+    return gulp.src(config.mainFile)
         .pipe(uglify())
         .pipe(rename({
             extname: '.min.js'
         }))
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['build']);
 });
 
 // Default Task
