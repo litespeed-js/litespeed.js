a6541abdacb633ee83cc26f59066ef77d6437672
eldad fux <eldad.fux@gmail.com>
2015-06-19 20:50:25 +0300

    build...

--- example/scripts/litespeed.js
+++ example/scripts/litespeed.js
@@ -39,6 +39,8 @@ var router      = require("router"),
  */
 var container = function() {
 
+    // FIXME add support for singelton
+
     var stock = [];
 
     return {
@@ -323,7 +325,7 @@ var view = function() {
         render: function(scope, container) {
             var view = this;
 
-            for (var key in stock) {
+            for (var key in stock) { // Iterate all registered views
                 if (stock.hasOwnProperty(key)) {
                     var value       = stock[key],
                         elements    = scope.querySelectorAll('[' + value.selector + ']'),
@@ -471,6 +473,18 @@ view.add({
 var view = require('view');
 
 view.add({
+    name: 'ls-dnd',
+    selector: 'data-ls-dnd',
+    template: false,
+    controller: function(element) {
+
+
+    }
+});
+
+var view = require('view');
+
+view.add({
     name: 'ls-eval',
     selector: 'data-ls-eval',
     template: false,
@@ -510,7 +524,7 @@ view.add({
 
                 element.innerHTML = output;
             }
-            ;
+        ;
 
         if(typeof array !== 'array' && typeof array !== 'object') {
             throw new Error('Reference \'' + path + '\' value must be array or object. ' + (typeof array) + ' given');
@@ -521,6 +535,77 @@ view.add({
         Object.observe(watch, function(changes) {
             render(element, array);
         });
+
+        var dragSrcEl = null;
+
+        function handleDragStart(e) {
+            // Target (this) element is the source node.
+            this.style.opacity = '0.4';
+
+            dragSrcEl = this;
+
+            e.dataTransfer.effectAllowed = 'move';
+            e.dataTransfer.setData('text/html', this.innerHTML);
+        }
+
+        function handleDragOver(e) {
+            if (e.preventDefault) {
+                e.preventDefault(); // Necessary. Allows us to drop.
+            }
+
+            e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+            return false;
+        }
+
+        function handleDragEnter(e) {
+            // this / e.target is the current hover target.
+            this.classList.add('over');
+        }
+
+        function handleDragLeave(e) {
+            this.classList.remove('over');  // this / e.target is previous target element.
+        }
+
+        function handleDrop(e) {
+            // this/e.target is current target element.
+
+            if (e.stopPropagation) {
+                e.stopPropagation(); // Stops some browsers from redirecting.
+            }
+
+            // Don't do anything if dropping the same column we're dragging.
+            if (dragSrcEl != this) {
+                // Set the source column's HTML to the HTML of the column we dropped on.
+                dragSrcEl.innerHTML = this.innerHTML;
+                this.innerHTML = e.dataTransfer.getData('text/html');
+            }
+
+            return false;
+        }
+
+        function handleDragEnd(e) {
+            this.style.opacity = '1';
+
+            // this/e.target is the source node.
+
+            [].forEach.call(element.children, function (node) {
+                node.classList.remove('over');
+            });
+        }
+
+        var start = function() {
+            [].forEach.call(element.children, function(node) {
+                node.addEventListener('dragstart',  handleDragStart, false);
+                node.addEventListener('dragenter',  handleDragEnter, false);
+                node.addEventListener('dragover',   handleDragOver, false);
+                node.addEventListener('dragleave',  handleDragLeave, false);
+                node.addEventListener('drop',       handleDrop, false);
+                node.addEventListener('dragend',    handleDragEnd, false);
+            });
+        };
+
+        start();
     }
 });
 
