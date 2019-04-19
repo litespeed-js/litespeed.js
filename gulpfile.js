56a708436f82f83dda40389979664a7f56f692c7
eldadfux <eldad.fux@gmail.com>
2019-04-19 22:30:01 +0300

    Updated gulp to version 4 + added jest for unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,103 @@
 // Include gulp
-var gulp        = require('gulp'),
+const { src, dest, series } = require('gulp');
 
-    // Plugins
-    concat      = require('gulp-concat'),
-    webserver   = require('gulp-webserver'),
-    jsmin       = require('gulp-jsmin'),
+// Plugins
+const gulpConcat = require('gulp-concat');
+const gulpWebserver = require('gulp-webserver');
+const gulpJsmin = require('gulp-jsmin');
 
-    // Config
-    config  = {
-        mainFile: 'litespeed.js',
-        src: [
-            'src/services/container.js',
-            'src/services/http.js',
-            'src/services/cookie.js',
-            'src/services/object.js',
-            'src/services/view.js',
-            'src/services/router.js',
-            'src/services/expression.js',
-            'src/services/filter.js',
-            'src/filters/escape.js',
-            'src/app.js',
-            'src/views/router.js',
-            'src/views/attrs.js',
-            'src/views/bind.js',
-            'src/views/if.js',
-            'src/views/loop.js',
-            'src/views/template.js',
-        ],
-        dest: './dist'
-    },
+// Config
+const config  = {
+    mainFile: 'litespeed.js',
+    src: [
+        'src/services/container.js',
+        'src/services/http.js',
+        'src/services/cookie.js',
+        'src/services/object.js',
+        'src/services/view.js',
+        'src/services/router.js',
+        'src/services/expression.js',
+        'src/services/filter.js',
+        'src/filters/escape.js',
+        'src/app.js',
+        'src/views/router.js',
+        'src/views/attrs.js',
+        'src/views/bind.js',
+        'src/views/if.js',
+        'src/views/loop.js',
+        'src/views/template.js',
+    ],
+    dest: './dist'
+};
 
-    // Config
-    configOld  = {
-        mainFile: 'litespeed.old.js',
-        src: [
-            'src/services/container.js',
-            'src/services/http.js',
-            'src/services/cookie.js',
-            'src/services/object.js',
-            'src/services/view.js',
-            'src/services/deprecated/form.js',
-            'src/services/router.js',
-            'src/services/expression.js',
-            'src/services/filter.js',
-            'src/filters/escape.js',
-            'src/app.js',
-            'src/views/router.js',
-            'src/views/attrs.js',
-            'src/views/if.js',
-            'src/views/loop.js',
-            'src/views/deprecated/alt.js',
-            'src/views/deprecated/class.js',
-            'src/views/deprecated/echo.js',
-            'src/views/deprecated/eval.js',
-            'src/views/deprecated/for.js',
-            'src/views/deprecated/hide.js',
-            'src/views/deprecated/href.js',
-            'src/views/deprecated/id.js',
-            'src/views/deprecated/print.js',
-            'src/views/deprecated/rerender.js',
-            'src/views/deprecated/selected.js',
-            'src/views/deprecated/src.js',
-            'src/views/deprecated/style.js',
-            'src/views/deprecated/template.js',
-            'src/views/deprecated/title.js',
-            'src/views/deprecated/trigger.js',
-            'src/views/deprecated/options.js',
-            'src/views/deprecated/placeholder.js',
-        ],
-        dest: './dist'
-    }
-;
+// Config
+const configOld  = {
+    mainFile: 'litespeed.old.js',
+    src: [
+        'src/services/container.js',
+        'src/services/http.js',
+        'src/services/cookie.js',
+        'src/services/object.js',
+        'src/services/view.js',
+        'src/services/deprecated/form.js',
+        'src/services/router.js',
+        'src/services/expression.js',
+        'src/services/filter.js',
+        'src/filters/escape.js',
+        'src/app.js',
+        'src/views/router.js',
+        'src/views/attrs.js',
+        'src/views/if.js',
+        'src/views/loop.js',
+        'src/views/template.js',
+        'src/views/deprecated/alt.js',
+        'src/views/deprecated/class.js',
+        'src/views/deprecated/echo.js',
+        'src/views/deprecated/eval.js',
+        'src/views/deprecated/for.js',
+        'src/views/deprecated/hide.js',
+        'src/views/deprecated/href.js',
+        'src/views/deprecated/id.js',
+        'src/views/deprecated/print.js',
+        'src/views/deprecated/rerender.js',
+        'src/views/deprecated/selected.js',
+        'src/views/deprecated/src.js',
+        'src/views/deprecated/style.js',
+        'src/views/deprecated/title.js',
+        'src/views/deprecated/trigger.js',
+        'src/views/deprecated/options.js',
+    ],
+    dest: './dist'
+};
 
-gulp.task('concat', function() {
-    return gulp.src(config.src)
-        .pipe(concat(config.mainFile))
-        .pipe(jsmin())
-        .pipe(gulp.dest(config.dest));
-});
+function concat () {
+    return src(config.src)
+        .pipe(gulpConcat(config.mainFile))
+        .pipe(gulpJsmin())
+        .pipe(dest(config.dest));
+}
 
-gulp.task('concat-old', function() {
-    return gulp.src(configOld.src)
-        .pipe(concat(configOld.mainFile))
-        .pipe(jsmin())
-        .pipe(gulp.dest(configOld.dest));
-});
+function concatOld () {
+    return src(configOld.src)
+        .pipe(gulpConcat(configOld.mainFile))
+        .pipe(gulpJsmin())
+        .pipe(dest(configOld.dest));
+}
 
-gulp.task('watch', function() {
-    gulp.watch(config.src, ['build']);
-});
+function demo () {
+    return src(config.dest + '/' + config.mainFile)
+        .pipe(dest('./example/scripts'));
+}
 
-gulp.task('demo', ['concat'], function () {
-    gulp.src(config.dest + '/' + config.mainFile)
-        .pipe(gulp.dest('./example/scripts'));
-});
-
-gulp.task('webserver', function() {
-    gulp.src('example')
-        .pipe(webserver({
+function webserver() {
+    return src('example')
+        .pipe(gulpWebserver({
             fallback: 'index.html'
         }));
-});
+}
 
-// Default Task
-gulp.task('build', ['concat', 'concat-old', 'demo']);
+exports.concat = concat;
+exports.concatOld = concatOld;
+exports.demo = demo;
+exports.webserver = webserver;
+exports.default = series(concat, concatOld, demo);
