8e0dbae359f3b3e86941301cd30a143eaa6f28b4
eldadfux <eldad.fux@gmail.com>
2019-04-19 08:03:46 +0300

    Changed how we init the framework

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+window.ls = window.ls || {};
+
 // Register all core services
 window.ls.container
     .set('window', window, true, false, false)
@@ -5,12 +7,10 @@ window.ls.container
     .set('element', window.document, true, false, false)
 ;
 
-window.ls.app = function(version) {
+/*window.ls.app = function() {
     return {
         run: function(window) {
             try {
-                window.ls.container.get('http').addGlobalParam('version', version);
-
                 // Trigger reclusive app rendering
                 this.view.render(window.document);
             }
@@ -23,4 +23,27 @@ window.ls.app = function(version) {
         container: window.ls.container,
         view: window.ls.container.get('view')
     }
+};*/
+
+window.ls.run = function (window) {
+    try {
+        // Trigger reclusive app rendering
+        this.view.render(window.document);
+    }
+    catch (error) {
+        let handler = window.ls.container.resolve(this.error);
+        handler(error);
+    }
 };
+
+window.ls.error = function() {
+    return function(error) {
+        console.error('ls-error', error.message, error.stack, error.toString());
+    }
+};
+
+window.ls.router = window.ls.container.get('router');
+
+window.ls.view = window.ls.container.get('view');
+
+window.ls.filter = window.ls.container.get('filter');
