89a0e0155881c08f662a457d2ac9fa875c1c2169
eldadfux <eldad.fux@gmail.com>
2019-04-20 10:17:15 +0300

    Added view service tests

---
+++ tests/services/view.test.js
@@ -0,0 +1,28 @@
+const app = require('../../dist/litespeed');
+
+const comp = {
+    'selector': 'data-comp',
+    'controller': function (element) {
+        element.innerHTML = element.getAttribute('data-comp');
+    }
+};
+
+document.body.innerHTML = '<div data-comp="hello world"></div>';
+
+const view = window.ls.view;
+
+test('adding a new view comp', () => {
+    expect(function () {
+        view.add(comp);
+
+        return view.stock[comp.selector];
+    }()).toEqual(comp);
+});
+
+test('rendering of a view comp', () => {
+    expect(function () {
+        view.render(document.body);
+
+        return document.body.innerHTML;
+    }()).toBe('<div>hello world</div>');
+});
