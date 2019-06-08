61372661453cf13a5703b2abeb805d25794be8ea
eldadfux <eldad.fux@gmail.com>
2019-06-08 10:01:30 +0300

    Added more tests to expression service

--- tests/services/expression.test.js
+++ tests/services/expression.test.js
@@ -26,8 +26,34 @@ test('expression parsing', () => {
     }()).toBe(tasks.title + ' & ' + tasks.config.theme.background);
 });
 
-test('expression parsing', () => {
+test('expression parsing - get paths', () => {
     expect(function () {
         return expression.getPaths()
     }()).toEqual(['tasks.title', 'tasks.config.theme.background']);
+});
+
+test('expression parsing - not found (undefined default)', () => {
+    expect(function () {
+        return expression.parse('{{not.defined}}', 'undefined')
+    }()).toBe('undefined');
+});
+
+test('expression parsing - not found (null default)', () => {
+    expect(function () {
+        return expression.parse('{{not.defined}}', 'null')
+    }()).toBe('null');
+});
+
+test('expression parsing - not found (with casting)', () => {
+    expect(function () {
+        return expression.parse('{{tasks.title}}', '', null, null, true)
+    }()).toBe("'" + tasks.title + "'");
+});
+
+test('expression parsing - customize regex', () => {
+    expression.regex = /(--.*?--)/gi;
+
+    expect(function () {
+        return expression.parse('--tasks.title--')
+    }()).toBe(tasks.title);
 });
