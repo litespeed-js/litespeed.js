1ad4a248e4a67ef15c9e4cc6e810913a6950c28d
eldadfux <eldad.fux@gmail.com>
2019-06-08 10:07:08 +0300

    Added tests for cookie service

---
+++ tests/services/cookie.test.js
@@ -0,0 +1,12 @@
+const app = require('../../dist/litespeed');
+
+const container = window.ls.container;
+const cookie = container.get('cookie');
+
+test('set/get cookie', () => {
+    expect(function () {
+        cookie.set('key', 'value', 100);
+
+        return cookie.get('key');
+    }()).toBe('value');
+});
