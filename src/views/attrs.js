3e9beeb08f0815a4adedb73bf72f16569715d829
eldadfux <eldad.fux@gmail.com>
2019-08-13 10:31:30 +0300

    Added option to set a key only attribute

--- src/views/attrs.js
+++ src/views/attrs.js
@@ -7,13 +7,16 @@ window.ls.container.get('view').add({
             for(let i = 0; i < attrs.length; i++) {
                 let attr    = attrs[i];
 
-                let key = expression.parse(attr.substring(0, attr.indexOf('=')), null, $as, $prefix) || null;
+                let key = expression.parse((attr.substring(0, attr.indexOf('=')) || attr), null, $as, $prefix);
 
                 paths = paths.concat(expression.getPaths());
 
-                let value = expression.parse(attr.substring(attr.indexOf('=') + 1), null, $as, $prefix) || null;
+                let value = '';
 
-                paths = paths.concat(expression.getPaths());
+                if(attr.indexOf('=') > -1) { // if value is actually set
+                    value = expression.parse(attr.substring(attr.indexOf('=') + 1), null, $as, $prefix) || '';
+                    paths = paths.concat(expression.getPaths());
+                }
 
                 if(!key) {
                     return null;
