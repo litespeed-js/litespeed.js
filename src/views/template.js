1aaacd2d867e3e6b4f2498f9d11ebf051d11d079
Eldad Fux <eldad.fux@gmail.com>
2020-04-03 22:08:44 +0300

    Added data binding for ls-temaplate view

--- src/views/template.js
+++ src/views/template.js
@@ -2,47 +2,66 @@ window.ls.container.get('view').add({
     selector: 'data-ls-template',
     template: false,
     repeat: true,
-    controller: function(element, view, http, expression, document) {
-        let template    = expression.parse(element.getAttribute('data-ls-template'));
+    controller: function(element, view, http, expression, document, container) {
+        let template    = element.getAttribute('data-ls-template') || '';
         let type        = element.getAttribute('data-type') || 'url';
+        let paths       = [];
+        let check       = function(init = false) {
+            let source = expression.parse(template);
+            paths= expression.getPaths();
+            element.innerHTML = '';
 
-        element.innerHTML = '';
+            if('script' === type) {
+                let inlineTemplate = document.getElementById(source);
 
-        if('script' === type) {
-            let inlineTemplate = document.getElementById(template);
+                if(inlineTemplate && inlineTemplate.innerHTML) {
+                    element.innerHTML = inlineTemplate.innerHTML;
 
-            if(inlineTemplate && inlineTemplate.innerHTML) {
-                element.innerHTML = inlineTemplate.innerHTML;
-
-                element.dispatchEvent(new CustomEvent('template-loaded', {
-                    bubbles: true,
-                    cancelable: false //TODO check if we need this set to false or true. might help improve performance
-                }));
-            }
-            else {
-                element.innerHTML = '<span style="color: red">Missing template "' + template + '"</span>';
-            }
-
-            return;
-        }
-
-        http
-            .get(template)
-            .then(function(element) {
-                    return function (data) {
-                        element.innerHTML = data;
-
-                        view.render(element);
-
-                        element.dispatchEvent(new CustomEvent('template-loaded', {
-                            bubbles: true,
-                            cancelable: false //TODO check if we need this set to false or true. might help improve performance
-                        }));
-                    }
-                }(element),
-                function() {
-                    throw new Error('Failed loading template');
+                    element.dispatchEvent(new CustomEvent('template-loaded', {
+                        bubbles: true,
+                        cancelable: false //TODO check if we need this set to false or true. might help improve performance
+                    }));
                 }
-            );
+                else {
+                    element.innerHTML = '<span style="color: red">Missing template "' + source + '"</span>';
+                }
+
+                if(!init) {
+                    view.render(element);
+                }
+
+                return;
+            }
+
+            http
+                .get(source)
+                .then(function(element) {
+                        return function (data) {
+                            element.innerHTML = data;
+
+                            view.render(element);
+
+                            element.dispatchEvent(new CustomEvent('template-loaded', {
+                                bubbles: true,
+                                cancelable: false //TODO check if we need this set to false or true. might help improve performance
+                            }));
+                        }
+                    }(element),
+                    function() {
+                        throw new Error('Failed loading template');
+                    }
+                );  
+        };
+
+        check(true);
+
+        for(let i = 0; i < paths.length; i++) {
+            let path = paths[i].split('.');
+            
+            while(path.length) {
+                container.bind(element, path.join('.'), check);
+                path.pop();
+            }
+        }
     }
 });
