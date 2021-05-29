dddc7eb82a38e842cd3e404d61c574728883d162
Eldad Fux <eldad.fux@gmail.com>
2021-05-29 11:41:16 +0300

    Fixed escape filter

--- src/filters/escape.js
+++ src/filters/escape.js
@@ -1,9 +1,9 @@
-window.ls.container.get('filter').add('escape', value => {
-    if(typeof value !== 'string') {
-        return value;
+window.ls.container.get('filter').add('escape', $value => {
+    if(typeof $value !== 'string') {
+        return $value;
     }
 
-    return value.replace(/&/g, '&amp;')
+    return $value.replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/\"/g, '&quot;')
